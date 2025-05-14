d4501e2fa0079b77d5b2a0ad9bee97e246786bb8
Emily Sadler <emily@sadler.gg>
2025-05-14 16:18:59 +0100

    fixed the image for the overview

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -104,19 +104,10 @@ function LandingPage({ onStartGame }) {
             <div className="intro-content">
               <div className="tab-grid">
                 <div className="tab-image">
-                  <div className="feature-illustration">
-                    {/* This would be an illustration or screenshot */}
-                    <div className="simulator-preview">
-                      <div className="preview-header">Bootcamp Simulator</div>
-                      <div className="preview-content">
-                        <div className="preview-stats">
-                          <div>Coding Skill: 75%</div>
-                          <div>Energy: 80%</div>
-                          <div>Day: 15</div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  <img
+                    src="/assets/BootCampSimScreen.png"
+                    alt="Bootcamp Illustration"
+                  />
                 </div>
                 <div className="tab-text">
                   <h2>The Bootcamp Experience, Gamified</h2>
@@ -205,7 +196,7 @@ function LandingPage({ onStartGame }) {
                   </div>
                 </div>
                 <div className="tab-image">
-                  <Calendar page="LandingPage" />
+                  <Calendar page="LandingPage" day={1} />
                 </div>
               </div>
             </div>
